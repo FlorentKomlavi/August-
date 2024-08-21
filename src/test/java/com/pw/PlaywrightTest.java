cc99842d786674b4a03375521e215f0608125a96
Florent <93553857+FlorentKomlavi@users.noreply.github.com>
2024-08-21 14:47:25 -0400

    first commit

---
+++ src/test/java/com/pw/PlaywrightTest.java
@@ -0,0 +1,32 @@
+package com.pw;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLOutput;
+
+public class PlaywrightTest {
+    @Test
+    public void firstPlaywrightScript() {
+
+        try (Playwright playwright = Playwright.create()) {
+            BrowserType browserType = playwright.chromium();
+            Browser browser = browserType.launch();
+            Page page = browser.newPage();
+            page.navigate("https://playwright.dev");
+            System.out.println(page.title());
+        }
+    }
+
+    @Test
+    public void firstPlaywrightScriptRefactored() {
+        try (Playwright playwright = Playwright.create()) {
+            Page page = playwright.chromium().launch().newPage();
+            page.navigate("https://playwright.dev");
+            System.out.println(page.title());
+        }
+    }
+}
---
+++ src/test/java/com/pw/Text_Selector.java
@@ -0,0 +1,26 @@
+package com.pw;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Text_Selector {
+    String home = "file:///" + System.getProperty("user.dir") + "\\index.html";
+    @Test
+    public  void    textSelector()
+    {
+        try (Playwright pw = Playwright.create())
+        {
+            BrowserType browsertype = pw.chromium();
+            Browser browser = browsertype.launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(2000));
+            Page page = browser.newPage();
+            page.navigate(home);
+
+            page.click("text = Accueil");
+            Assertions.assertEquals(page.title(), "Les Legumes du Terroir");
+        }
+    }
+}
